b7d65322ef7f4ecb6a7d11d1238cbc34d1ffefe5
Joshua Ramirez <jramirezmalerv@gmail.com>
2023-09-28 13:08:31 -0600

    Reverted all code back to normal: Tests successful

--- src/Cat.java
+++ src/Cat.java
@@ -30,11 +30,11 @@ public class Cat {
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-//		g2.setColor(Color.green);
-//		x = catX + EYE_X; 
-//		y = catY + EYE_Y;
-//		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-//		x += EYE_SEPARATION;
+		g2.setColor(Color.green);
+		x = catX + EYE_X; 
+		y = catY + EYE_Y;
+		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
+		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
 		g2.setColor(Color.pink);
